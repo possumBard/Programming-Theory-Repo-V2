955fa274e802eda8505f3fe67a952b456fe0e84b
possumBard <107641206+possumBard@users.noreply.github.com>
2022-06-25 12:19:58 -0600

    Input name commit 1
    - Error message shows
    - Still need to check for a valid name

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -8,14 +8,42 @@ public class MenuUIHandler : MonoBehaviour
 {
     public TextMeshProUGUI userName;
     public TMP_InputField userInput;
+    public TextMeshProUGUI errorMessage;
+
+    public bool validName;
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        if (validName)
+        {
+            SceneManager.LoadScene(1);
+            Debug.Log(userInput.text.Length);
+            Debug.Log(userName.text);
+        }
+        else
+        {
+            errorMessage.gameObject.SetActive(true);
+            errorMessage.text = "Error: Valid names are between 1 and 4 characters long.";
+            ResetName();
+        }
+        
     }
 
     public void SetName()
     {
-        userName.text = userInput.text;
+        if(userInput.text.Length > 4 && userInput.text.Length > 0)
+        {
+            userName.text = userInput.text;
+            validName = true;
+        }
+        else
+        {
+            validName = false;
+        }
+    }
+
+    public void ResetName()
+    {
+        userName.text = "";
     }
 }
